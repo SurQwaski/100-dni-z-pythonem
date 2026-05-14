f9eea8887ac20fc4f244f447787929f36b008197
Noe <kacper.surqwaski@gmail.com>
2026-05-14 16:42:08 +0200

    dzien 1 ukonczony

--- dzien 1/main.py
+++ dzien 1/main.py
@@ -1,1 +1,7 @@
-print("Radosław")
+# Rozwiązanie pewnego zadania wykorzystujące tylko jedną linijkę. Takie rozwiązania są krótsze, natomiast zdecydowanie mniej czytelne. Nie zawsze krócej oznacza lepiej.
+# print("Your name has", len(input("What is your name? ")) ,"characters.")
+
+print("Welcome to the Band Name Generator.")
+user_city = input("What is the name of the city that you grew up in?\n")
+user_pet_name = input("What is your pet's name?\n")
+print("You could name your band " + user_city + " " + user_pet_name + ".")
